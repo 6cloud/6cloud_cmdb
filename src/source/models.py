883a751190cf95bb7b1d5e9a5a6340c85ef3981b
liuhuipy <liuhui_py@163.com>
2018-08-13 00:49:30 +0800

    Update source models

--- src/source/models.py
+++ src/source/models.py
@@ -12,7 +12,54 @@ class BaseModel(models.Model):
     is_deleted = models.BooleanField(default=False, blank=True, null=True, verbose_name='是否被删除')
     desc = models.TextField(blank=True, null=True, verbose_name='描述')
     create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
-    update_time = models.DateTimeField(blank=True, null=True, verbose_name='更新时间')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='更新时间')
+
+    class Meta:
+        abstract = True
+
+
+class Business(BaseModel):
+    name = models.CharField(max_length=32, unique=True, verbose_name='业务名')
+    leader = models.ForeignKey(User, verbose_name='负责人')
+
+    class Meta:
+        verbose_name = '业务'
+        verbose_name_plural = verbose_name
+        permissions = (
+            ('list_business', ('获取业务列表')),
+            ('get_business', ('获取业务信息')),
+            ('add_business', ('添加业务')),
+            ('change_business', ('修改业务信息')),
+            ('delete_business', ('删除业务')),
+        )
+        default_permissions = ()
+
+    def __str__(self):
+        return self.name
+
+
+class Application(BaseModel):
+    name = models.CharField(max_length=32, unique=True, verbose_name='应用')
+    business = models.ForeignKey(Business, related_name='application_business', verbose_name='所属业务')
+    ops = models.ForeignKey(User, related_name='application_ops', verbose_name='运维负责人')
+    dev = models.ForeignKey(User, related_name='application_dev', blank=True, null=True, verbose_name='研发负责人')
+    qa = models.ForeignKey(User, related_name='application_qa', blank=True, null=True, verbose_name='测试负责人')
+    cs = models.ForeignKey(User, related_name='application_cs', blank=True, null=True, verbose_name='安全负责人')
+
+    class Meta:
+        verbose_name = '业务'
+        verbose_name_plural = verbose_name
+        permissions = (
+            ('list_application', ('获取应用列表')),
+            ('get_application', ('获取应用信息')),
+            ('add_application', ('添加应用')),
+            ('change_application', ('修改应用信息')),
+            ('delete_application', ('删除应用')),
+        )
+        default_permissions = ()
+
+    def __str__(self):
+        return self.name
 
 
 class SystemUser(BaseModel):
@@ -25,13 +72,25 @@ class SystemUser(BaseModel):
     class Meta:
         verbose_name = '系统用户'
         verbose_name_plural = verbose_name
+        permissions = (
+            ('list_systemuser', ('获取系统用户列表')),
+            ('get_systemuser', ('获取系统用户信息')),
+            ('add_systemuser', ('添加系统用户')),
+            ('change_systemuser', ('修改系统用户信息')),
+            ('delete_systemuser', ('删除系统用户')),
+        )
+        default_permissions = ()
+
+    def __str__(self):
+        return self.name
 
 
-class HostGroup(BaseModel):
+class Colony(BaseModel):
     """
     HostGroup model
     """
-    name = models.CharField(max_length=64, unique=True, verbose_name='主机组名')
+    name = models.CharField(max_length=64, unique=True, verbose_name='集群名')
+    application = models.ForeignKey(Application, related_name='colony_application', verbose_name='所属应用')
 
     class Meta:
         verbose_name = '主机组'
@@ -62,10 +121,10 @@ class IDC(BaseModel):
         verbose_name = '机房'
         verbose_name_plural = verbose_name
         permissions = (
-            ('list_idc', ('获取机房')),
-            ('get_idc', ('获取机房')),
+            ('list_idc', ('获取机房列表')),
+            ('get_idc', ('获取机房信息')),
             ('add_idc', ('添加机房')),
-            ('change_idc', ('修改机房')),
+            ('change_idc', ('修改机房信息')),
             ('delete_idc', ('删除机房')),
         )
         default_permissions = ()
@@ -78,7 +137,27 @@ class Cabinet(BaseModel):
     """
     Cabinet model
     """
-    pass
+    idc = models.ForeignKey(IDC, related_name='cabinet_idc', verbose_name='所属机房')
+    address = models.CharField(max_length=128, unique=True, verbose_name='所在机房位置')
+    unum = models.IntegerField(default=0, verbose_name='机柜U个数')
+    hosts = models.ManyToManyField('Host', related_name='cabinet_hosts', verbose_name='主机')
+    status = models.IntegerField(default=0, verbose_name='状态')
+
+    class Meta:
+        verbose_name = '机柜'
+        verbose_name_plural = verbose_name
+        permissions = (
+            ('list_cabinet', ('获取机柜列表')),
+            ('get_cabinet', ('获取机柜信息')),
+            ('add_cabinet', ('添加机柜')),
+            ('change_cabinet', ('修改机柜信息')),
+            ('delete_cabinet', ('删除机柜')),
+        )
+        default_permissions = ()
+
+    def __str__(self):
+        return self.address
+
 
 
 class Mem(BaseModel):
@@ -117,9 +196,12 @@ class Host(BaseModel):
         ('virtual server', 'Virtual Server'),
     )
     HOST_STATUS = (
-        (1, '使用'),
-        (2, '空闲'),
-        (3, '故障'),
+        (1, '使用中'),
+        (2, '空闲中'),
+        (3, '故障中'),
+        (4, '测试机'),
+        (5, '开发机'),
+        (6, '维修中')
     )
     hostname = models.CharField(max_length=64, unique=True, verbose_name='主机名')
     intranet_ipaddress = models.GenericIPAddressField(unique=True, verbose_name='内网IP')
@@ -128,7 +210,7 @@ class Host(BaseModel):
     macaddress = models.CharField(max_length=32, blank=True, null=True, verbose_name='Mac地址')
     sn = models.CharField(max_length=128, blank=True, null=True, verbose_name='SN号')
     manufacturer = models.CharField(max_length=64, blank=True, null=True, verbose_name='厂商')
-    hostgroup = models.ManyToManyField(HostGroup, related_name='host_groups', verbose_name='主机组')
+    Colony = models.ManyToManyField(Colony, related_name='host_groups', verbose_name='主机组')
 
     port = models.SmallIntegerField(default=22, blank=True, null=True, verbose_name='端口')
     os_type = models.CharField(max_length=32, blank=True, null=True, verbose_name='系统类型')
@@ -159,46 +241,3 @@ class Host(BaseModel):
     def __str__(self):
         return self.hostname
 
-
-class Business(BaseModel):
-    name = models.CharField(max_length=32, unique=True, verbose_name='业务名')
-    leader = models.ForeignKey(User, verbose_name='负责人')
-
-    class Meta:
-        verbose_name = '业务'
-        verbose_name_plural = verbose_name
-        permissions = (
-            ('list_host', ('获取主机列表')),
-            ('get_host', ('获取主机信息')),
-            ('add_host', ('添加主机')),
-            ('change_host', ('修改主机信息')),
-            ('delete_host', ('删除主机')),
-        )
-        default_permissions = ()
-
-    def __str__(self):
-        return self.name
-
-
-class Application(BaseModel):
-    name = models.CharField(max_length=32, unique=True, verbose_name='应用')
-    business = models.ForeignKey(Business, related_name='application_business', verbose_name='所属业务')
-    ops = models.ForeignKey(User, related_name='application_ops', verbose_name='运维负责人')
-    dev = models.ForeignKey(User, related_name='application_dev', blank=True, null=True, verbose_name='研发负责人')
-    qa = models.ForeignKey(User, related_name='application_qa', blank=True, null=True, verbose_name='测试负责人')
-    cs = models.ForeignKey(User, related_name='application_cs', blank=True, null=True, verbose_name='安全负责人')
-
-    class Meta:
-        verbose_name = '业务'
-        verbose_name_plural = verbose_name
-        permissions = (
-            ('list_host', ('获取主机列表')),
-            ('get_host', ('获取主机信息')),
-            ('add_host', ('添加主机')),
-            ('change_host', ('修改主机信息')),
-            ('delete_host', ('删除应用')),
-        )
-        default_permissions = ()
-
-    def __str__(self):
-        return self.name
