b69c07a239a1ad2dc487853aa992e7ba8a0908ac
liuhuipy <liuhui_py@163.com>
2018-08-17 17:55:31 +0800

    add system model serializer

--- cmdb/system/serializers.py
+++ cmdb/system/serializers.py
@@ -1,1 +1,18 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
+
+from rest_framework import serializers
+
+from system.models import User, Role
+
+
+class RoleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Role
+        fields = ('id', 'name', 'permission', 'create_time', 'update_time')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    user_roles = RoleSerializer(many=True, read_only=True, required=False)
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'user_roles', 'is_active', 'is_superuser')
