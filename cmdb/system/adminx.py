ef6eb14de62f54a93d5c3af028a822b8930ebf4b
liuhuipy <liuhui_py@163.com>
2018-08-17 11:25:30 +0800

    fix user model and source model.

--- cmdb/system/adminx.py
+++ cmdb/system/adminx.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+import xadmin
 
-# Register your models here.
+from system.models import User, Role
+
+
+class RoleAdmin:
+    list_display = ['id', 'name', 'permission', 'create_time', 'update_time']
+    search_fields = ('id', 'name', 'permission', 'create_time', 'update_time')
+    list_filter = ('id', 'name', 'permission', 'create_time', 'update_time')
+    style_fields = {'permission': 'm2m_transfer'}
+
+class UserAdmin:
+    list_display = ['id', 'username', 'email', 'role']
+    search_fields = ('id', 'username', 'email', 'role')
+    list_filter = ('id', 'username', 'email', 'role')
+    style_fields = {'role': 'm2m_transfer'}
+
+
+xadmin.site.unregister(User)
+xadmin.site.register(Role, RoleAdmin)
+xadmin.site.register(User, UserAdmin)
